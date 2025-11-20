b73bbbab02191fbbb504b53a79ce2f6fc450d837
fanyang <fan.yang@syntalon.com>
2025-11-19 21:47:27 -0500

    change FIX acceptor version and consolidate config class

--- backend/src/main/java/com/longvin/trading/config/FixConfiguration.java
+++ backend/src/main/java/com/longvin/trading/config/FixConfiguration.java
@@ -1,9 +1,18 @@
 package com.longvin.trading.config;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.xml.parsers.DocumentBuilderFactory;
 
 @Configuration
 @EnableConfigurationProperties(FixClientProperties.class)
 public class FixConfiguration {
+    @Bean
+    public DocumentBuilderFactory documentBuilderFactory() {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setAttribute("http://javax.xml.XMLConstants/property/accessExternalDTD", "all");
+        return factory;
+    }
 }
