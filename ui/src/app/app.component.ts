7e96f9559e1d140301428e67f0d1c5447b74e7e9
fanyang <fan.yang@syntalon.com>
2025-12-29 10:12:11 -0500

    adding log page in ui

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [RouterModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
